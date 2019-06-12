54ea2edd8f943aa226acbb1f9e63e103548c1581
Eric Volpert <ericvolp12@gmail.com>
2019-06-12 05:08:50 +0000

    Working waterfall

--- src/components/root.js
+++ src/components/root.js
@@ -19,29 +19,45 @@ class RootComponent extends React.Component {
     };
   }
 
-  componentDidMount() {
+  componentWillMount() {
     prepareData('./data/songs.csv', this.state.metaProps, this.state.traits).then((res) => this.setState({
       isLoaded: true,
       data: res,
-      songs: res.songs,
-      selectedSongs: res.songs
+      songs: res.songs.map((v) => {
+        v.selected = true;
+        return v;
+      }),
+      scales: res.scales
     }));
+  }
+  selectSong(index) {
+    const songs = this.state.songs;
+    songs[index].selected = true;
+    this.setState({songs});
+  }
+
+  deselectSong(index) {
+    const songs = this.state.songs;
+    songs[index].selected = false;
+    this.setState({songs});
   }
 
   render() {
-    console.log(this.state)
     return (
       <div className="relative">
-        <Waterfall
-          songs={this.state.songs}
-          selectedSongs={this.state.selectedSongs}
-          updateSelectedSongs={songs => this.setState({selectedSongs: songs})}
-          traits={this.state.traits}
-          scales={this.state.data.scales}
-          height={1000}
-          width={600}
-        />
-        <Feature data={this.state.songs} selectedSongs={this.state.selectedSongs}/>
+        {this.state.data ?
+          <Waterfall
+            songs={this.state.songs}
+            selectSong={index => this.selectSong(index)}
+            deselectSong={index => this.deselectSong(index)}
+            traits={this.state.traits}
+            scales={this.state.scales}
+            height={1500}
+            width={800}
+          /> : null }
+        <Feature data={this.state.songs} selectSong={(i) => {
+          
+        }}/>
         <Median data={this.state.songs} selectedSongs={this.state.selectedSongs}/>
         <WhoComponent/>
       </div>
--- src/components/waterfall.js
+++ src/components/waterfall.js
@@ -5,73 +5,37 @@ import {scaleLinear, scaleBand} from 'd3-scale';
 import {axisBottom, axisLeft} from 'd3-axis';
 import {select} from 'd3-selection';
 
-/*
-*   Given a data object, generates a waterfall of the songs inside.
-*   The order of the columns (L->R) is as follows: valence, liveness, accousticness, energy, danceability.
-*   INPUT: data (Object)
-*     svg (Object)
-*     height (Number)
-*     width (Number)
-*     traits (Array)
-*   OUTPUT: Nothing returned but should draw a waterfall on svg
-*/
-function drawWaterfall(svg, data, height, width, traits) {
-  const songs = data.songs;
-  const x = scaleLinear()
-    .domain([0, 1])
-    .range([0, width]);
-
-  const songNames = scaleBand()
-    .domain(songs.map(d => d.name))
-    .range([0, height])
-    .padding(0.1);
-
-  // append the rectangles for the bar chart
-  svg.selectAll('.waterfallBar')
-    .data(songs)
-    .enter().append('g')
-    .attr('class', 'waterfallBar')
-    .attr('width', width)
-    .attr('y', d => songNames(d.name))
-    .attr('height', songNames.bandwidth())
-    .selectAll('.colorBar')
-    .data(traits)
-    .enter()
-    .append('rect')
-    .attr('class', 'colorBar');
-  // Go over our bars and create the new bars inside each
-  // This was a massive pain to figure out without just hard coding every bar
-  // It looks a lot shorter now that I found a clean way to do it lmao I hate my life
-  svg.selectAll('.waterfallBar')
-    .each(function(parentDatum) {
-      const parent = select(this);
-      parent.selectAll('.colorBar').each(function(childDatum, i) {
-        select(this).attr('width', x(1 - (i * 0.2)))
-          .attr('height', songNames.bandwidth())
-          .attr('y', parent.attr('y'))
-          .attr('fill', data.scales[childDatum](parentDatum[childDatum]));
-      });
-    });
-
-  // add the songNames Axis
-  svg.append('g')
-    .call(axisLeft(songNames));
-
-  const traitsOrdered = traits.reverse().map(word => word.toUpperCase());
-  const xAxis = scaleBand()
-    .domain(traitsOrdered)
-    .range([0, width]);
-
-  // add the x Axis
-  svg.append('g')
-    .attr('transform', `translate(0,${ height })`)
-    .call(axisBottom(xAxis));
-}
-
 export default class Waterfall extends Component {
   constructor(props) {
     super(props);
-    this.state = props;
+    const x = scaleLinear()
+      .domain([0, 1])
+      .range([0, props.width - 200]);
+
+    const songNames = scaleBand()
+      .domain(props.songs.map(d => d.name))
+      .range([0, props.height - 20])
+      .padding(0.1);
+
+    this.state = {
+      x,
+      songNames
+    };
+  }
+
+  componentDidMount() {
+    this.renderAxis();
+  }
+
+  componentDidUpdate() {
+    this.renderAxis();
+  }
+
+  renderAxis() {
+    const yAxis = axisLeft(this.state.songNames);
+    const xAxis = axisBottom(this.state.x);
+    select(this.songAxisElement).call(axisG => axisG.call(yAxis));
+    select(this.traitAxisElement).call(axisG => axisG.call(xAxis));
   }
 
   render() {
@@ -81,39 +45,50 @@ export default class Waterfall extends Component {
       songs,
       traits,
       scales,
-      updateSelectedSongs,
-      selectedSongs
+      selectSong,
+      deselectSong
     } = this.props;
-    const x = scaleLinear()
-      .domain([0, 1])
-      .range([0, width]);
+    const {
+      x,
+      songNames
+    } = this.state;
 
-    const songNames = scaleBand()
-      .domain(songs.map(d => d.name))
-      .range([0, height])
-      .padding(0.1);
     return (
       <div className={'waterfall_container'}>
         <svg className={'waterfall_svg'} width={width} height={height}>
+          <g
+            ref={(el) => {
+              this.songAxisElement = el;
+            }}
+            transform={'translate(195)'}/>
           {songs.map((song, idx) => {
             return (
               <g className="waterfallBar"
-                width={width}
+                key={`g_${song.name}`}
+                width={width - 200}
                 height={songNames.bandwidth()}
-                y={songNames(song.name)
-              }>
+                y={songNames(song.name)}
+                transform={'translate(200)'}
+                onClick={d => song.selected ? deselectSong(idx) : selectSong(idx)}
+              >
                 {traits.map((trait, jdx) => {
                   return (
                     <rect
+                      key={`rect_${song.name}_${trait}`}
                       height={songNames.bandwidth()}
                       y={songNames(song.name)}
                       width={x(1 - (jdx * 0.2))}
-                      fill={scales[trait](song[trait])} />
+                      fill={song.selected ? scales[trait](song[trait]) : 'black'} />
                   );
                 })}
               </g>
             );
           })}
+          <g
+            ref={(el) => {
+              this.traitAxisElement = el;
+            }}
+            transform={`translate(200, ${height - 20})`}/>
         </svg>
       </div>
     );
